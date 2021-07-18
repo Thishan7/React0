ab6af8fa4dfbdbf73f69428fd6d9592b4f281c97
Thishan96 <2018cs039@stu.ucsc.cmb.ac.lk>
2021-07-19 05:25:51 +0530

    deleted delete button from the blogs

--- src/BlogList.js
+++ src/BlogList.js
@@ -6,8 +6,6 @@ const BlogList = (props) => {
         <div className="class-list">
             <h2>{props.title}</h2>
              {blogs.map((blog) => (<div className="blog-preview" key={blog.id} > <h2>{ blog.title }</h2> <p>Written by { blog.author }</p> 
-             {/* 3 calling handle delete function by passing blog id*/}
-             <button onClick={()=> props.handleDelete(blog.id)}>Delete blog</button>
              </div>))}
             
         </div>
