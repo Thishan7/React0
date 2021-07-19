a777719c00f7ab659ec35cb446e615a31c574f2f
Thishan96 <2018cs039@stu.ucsc.cmb.ac.lk>
2021-07-19 21:38:44 +0530

    error messages popups done. (frontend to server, server to frontend)

--- src/Home.js
+++ src/Home.js
@@ -6,20 +6,33 @@ const Home = () => {
 const title1 = "All blogs";  
 const [blogs, setBlogs] = useState(null)
 const [isPendig,setIsPending] = useState(true);
+const [error,setError] = useState(null);
 
 useEffect (()=>{
   setTimeout(() => {
     fetch("http://localhost:8000/blogs")
+
     .then(res => {
+      //if data coudl not reach to the resource from the server
+      if(!res.ok){
+        throw Error('data didnt reach to the page from server or page not available');
+      }
       return res.json();
     })
+
     .then(data =>{
       console.log(data);
       setBlogs(data);
       setIsPending(false);
     })
-  
-  }, 2000);
+
+    //catch any kind of network error (if we cannot reach the server this fires)
+    .catch(err => {
+      setIsPending(false);
+      //set error message using setError() function, This catch any 'throw Error' property too
+      setError(err.message);
+    })
+  }, 1000);
  },[])
 
 
@@ -29,6 +42,7 @@ console.log("use effect run");
 
   return (
     <div className="home">
+      {error&&<div>{error}</div>} 
       {isPendig && <h1>Loading.....</h1>}
         {/* passing data to other js fle */}
       {blogs && <BlogList blogs={blogs} title={title1} />}
